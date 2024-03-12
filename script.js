0e2fdd6490eb999c5bb6818ffc018e05d4171e30
Paras Ahuja <parasxahuja@gmail.com>
2024-03-12 23:58:01 +0530

    add navbar and scroll animations

--- script.js
+++ script.js
@@ -1,48 +1,38 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('#main'),
-//     smooth: true
-// });
-
-function locomotiveAnimation(){
-    gsap.registerPlugin(ScrollTrigger);
-
+    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
     // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
     const locoScroll = new LocomotiveScroll({
-    el: document.querySelector("#main"),
-    smooth: true,
+        el: document.querySelector("#main"),
+        smooth: true,
     });
     // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
     locoScroll.on("scroll", ScrollTrigger.update);
 
     // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
     ScrollTrigger.scrollerProxy("#main", {
-    scrollTop(value) {
-        return arguments.length
-        ? locoScroll.scrollTo(value, 0, 0)
-        : locoScroll.scroll.instance.scroll.y;
-    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-    getBoundingClientRect() {
-        return {
-        top: 0,
-        left: 0,
-        width: window.innerWidth,
-        height: window.innerHeight,
-        };
-    },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector("#main").style.transform
-        ? "transform"
-        : "fixed",
+        scrollTop(value) {
+            return arguments.length
+            ? locoScroll.scrollTo(value, 0, 0)
+            : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return {
+            top: 0,
+            left: 0,
+            width: window.innerWidth,
+            height: window.innerHeight,
+            };
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector("#main").style.transform
+            ? "transform"
+            : "fixed",
     });
-
     // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
     ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
     // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
     ScrollTrigger.refresh();
-}
-locomotiveAnimation()
 
 function navbarAnimation(){
     gsap.to("#navbar .links", {
@@ -56,10 +46,33 @@ function navbarAnimation(){
             scrub: true,
         },
     });
+    gsap.to("#logo img", {
+        transform: "translateY(-100%)",
+        delay:2.5,
+        scrollTrigger: {
+          trigger: "#page1",
+          scroller: "#main",
+          start: "top 0",
+          end: "top -5%",
+          scrub: true,
+        },
+      });
+
+    gsap.to("#go-to-home  ", {
+        transform: "translateY(-50%)",
+        scale:1,
+        duration:2.5,
+        scrollTrigger: {
+            trigger: "#page1",
+            scroller: "#main",
+            start: "top 0",
+            end: "top -5%",
+            scrub: true,
+        },
+    });
 }
 navbarAnimation();
     
-
 function videoHoverAnimation(){
     const videoContainer = document.getElementById("video-container")
     const heading = document.querySelector(".heading-container");
@@ -95,23 +108,19 @@ function videoHoverAnimation(){
 
     videoContainer.addEventListener("mousemove",mouseMove)
     heading.addEventListener("mousemove",mouseMove)
-
-    scrollbtn.addEventListener("click",function(){
-        window.scrollY('100vh')
-    })
     
 }
 videoHoverAnimation();
 
 function loadAnimation(){
     const page2 = document.getElementById("page2");
-    // gsap.from("#nav",{
-    //     y:-100,
-    //     opacity:0,
-    //     delay:1.2,
-    //     duration:0.9,
-    //     stagger:0.2,
-    // })
+    gsap.from("#nav",{
+        y:-100,
+        opacity:0,
+        delay:1.2,
+        duration:0.9,
+        stagger:0.2,
+    })
     gsap.from("#page1 h1",{
         y:100,
         opacity:0,
@@ -125,14 +134,14 @@ function loadAnimation(){
         delay:0.3,
         duration:0.9,
     })
-    
-        // gsap.from("#page2 .elem1",{
-        //     y:100,
-        //     opacity:0,
-        //     delay:0.5,
-        //     duation:0.8,
-        //     stagger:1.2
-        // })
+
+    // gsap.from("#page2 .elem1",{
+    //     y:100,
+    //     opacity:0,
+    //     delay:0.5,
+    //     duation:0.8,
+    //     stagger:1.2
+    // })
     
 }
 loadAnimation();
@@ -152,5 +161,35 @@ function cursorAnimation(){
             transform: `translate(-50%,-50%) scale(1)`
         })
     })   
+
+    // const button = document.querySelector("#go-to-h a")
+    // const target = button.getAttribute("href")
+    // const targetElement = document.querySelector("#target")
+    // const targetRect = targetElement.getBoundingClientRect();
+
+    
+    // button.addEventListener("click",(event)=>{
+    //     event.preventDefault();
+
+    //     console.log(button);
+    //     gsap.to(window,{
+    //         scrollTo:0,
+    //         // duration:1,
+    //         // ease: "power2.inOut"
+    //     })
+        
+    // })
 }
 cursorAnimation();
+
+
+document.querySelector("#scroll").addEventListener("click",(e)=>{
+    locoScroll.scrollTo(".category-heading",0,1000);
+})
+document.querySelector("#go-to-home").addEventListener("click",(details)=>{
+    locoScroll.scrollTo(0,0);
+})
+
+
+
+
